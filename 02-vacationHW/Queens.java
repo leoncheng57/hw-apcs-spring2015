06d1b93563a182757a66489e9857c49541d0ea26
leoncheng57 <57leonardo@gmail.com>
2015-02-17 18:56:51 -0500

    working on queens

--- 02-vacationHW/Queens.java
+++ 02-vacationHW/Queens.java
@@ -31,23 +31,46 @@ public class Queens{
 	return s;
     }
 
+    public boolean canPlace(int x, int y){
+	return check(x,y,-1,-1) && check(x,y,-1,0) && check(x,y,0,-1) && check(x,y,0,1) && check(x,y,1,0) && check(x,y,1,1);
+    }
+
+    public boolean check(int x, int y, int deltaX, int deltaY){
+	int otherX=x;
+	int otherY=y;
+	while (otherX<board.length && otherY<board.length && otherX>=0 && otherY>=0){
+	    if (board[otherX][otherY]!='#'){
+		return false;
+	    }
+	    otherX+=deltaX;
+	    otherY+=deltaY;
+	}
+	return true;
+    }
+    
     public void solve(int x, int y, int count){
 	if (count == board.length){
 	    solved = true;
 	}
-	if (){
+	if(!canPlace(x,y)){
 	    return;
 	}
 	count++;
-	solve(
+	for (int i = 0; i<board.length;i++){
+	    solve(x+i,y+1,count);
+	}
 	if (!solved){
 	    board[x][y]='#';
 	}
     }
+
+    public void solve(int x, int y){
+	solve(x,y,1);
+    }
     
     public static void main(String[] args){
 	Queens q = new Queens(5);
-	//q.solve();
+	q.solve(1,1);
 	System.out.println(q);
     }
 }
