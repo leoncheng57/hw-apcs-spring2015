f1514efbab2728cbec8d6614550a2146199f146c
leoncheng57 <57leonardo@gmail.com>
2015-02-25 18:36:10 -0500

    completed hw

--- 04-mergesort/mergesort.java
+++ 04-mergesort/mergesort.java
@@ -1,19 +1,20 @@
 import java.util.*;
 import java.io.*;
 
-public class mergesort{
-    /*
-      a and b are ArrayLists that are alread sorted from low to high.
-      The could be empty
+/*
+  a and b are ArrayLists that are alread sorted from low to high.
+  The could be empty
 
-      build and return a sorted ArrayList using the merge technique we
-      discussed in class
-    */
+  build and return a sorted ArrayList using the merge technique we
+  discussed in class
+*/
+
+public class mergesort{
     public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
 	ArrayList<Integer> ret = new ArrayList<Integer>();
 	int i = 0;
 	int k = 0;
-	while (i<a.size()&&k<b.size()){
+	while (i<a.size() && k<b.size()){
 	    if (a.get(i)<b.get(k)){
 		ret.add(a.get(i));
 		i++;
@@ -22,19 +23,23 @@ public class mergesort{
 		ret.add(b.get(k));
 		k++;
 	    }
+	    // System.out.printf("a:%d, b%d",a,b);
 	}
-	// if(i>a.size()){
-	//     ret = ret + a.subList(i,a.size());
-	// }
-	// if (k>b.size()){
-	//     ret = ret + b.subList(n,b.size());
-	// }
+	if(i<a.size()){
+	    for (i=i;i<a.size();i++)
+		ret.add(a.get(i));
+	}
+	if(k<b.size()){
+	    for (k=k;k<b.size();k++)
+		ret.add(b.get(k));
+	}	
+	return ret;
     }
 
     public static void main(String[] args){
-	mergesort obj = new mergesort();
-	ArrayList<Integer> a = new ArrayList<Integer>();
-	ArrayList<Integer> b = new ArrayList<Integer>();
-	obj.sort(a,b);
+	mergesort m = new mergesort();
+	ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(1,2,6,99));
+	ArrayList<Integer> b = new ArrayList<Integer>(Arrays.asList(0,4,5,7,8,9,12,51,65,78,98));
+	System.out.println(m.merge(a,b));
     }
 }
