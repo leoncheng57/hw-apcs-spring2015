15ea72ce68d34b1f61c89ee194d22e0e0d04885a
leoncheng57 <57leonardo@gmail.com>
2015-03-08 14:42:39 -0400

    working on hw

--- 05-selection/Sort.java
+++ 05-selection/Sort.java
@@ -2,47 +2,127 @@ import java.util.*;
 import java.io.*;
 
 public class Sort{
-    
-    int[] L; //input unsorted array
-    int[] D; //array I create to do stuff in
-    int pVal; //pivot value
 
-    public void pivot(int[] a){
-	L = a;
-	D = new int[a.length];
-	pVal = a[0];
-	pivot(a,0,a.length-1); 
+    private int[] arr = {5,10,1,3,4,8,9,2,7};    
+    //arr MUST HAVE NO DUPLICATE VALUES
+    
+    public void select(int k){
+	select(arr,k,0,arr.length-1);
     }
 
+    /*
+      A = the array
+      k = we are looking for the kth smallest element in arr 
+      L = original lower index
+      Li = lower index
+      H = original higher index
+      Hi = higher index
+      P = pivot value
+      Pi = pivot index
+     */
+    public void select(int[] A,int k,int L,int H){
+	
+	int P = A[L];
+	int Pi = L;
+	//leaving L and H untampered with 
+	int Li = L;
+	int Hi = H;
+	//swap pivot value with highest index 
+	A[Li] = A[Hi];
+	A[Hi] = P;
+	Pi=Hi;
+	Hi--;
 
-    public void pivot(int[] a, int si, int ei){
-	int i = 1; //si=1 because we are ignoring the first val of the array, the pVal
-	while (si != ei){
-	    if (L[i] < pVal){
-		D[si]=L[i];
-		si++;
+	print( Li, Hi, Pi, k, A);	    
+
+
+	//The main workload: 
+	//swapping things to the left or right side ofthe array
+	while (Hi>Li){
+	    if (A[Li]<P){
+		//just leave the element on the left side and move on
+		Li++;
 	    }
 	    else{
-		D[ei] = L[i];
-		ei--;
+		//swap higher element to the right side of the array
+		int tmp = A[Li];
+		A[Li] = A[Hi];
+		A[Hi]=tmp;
+		Hi--;
 	    }
-	    System.out.println(L[i]);
-	    System.out.println(this);
-	    i++;
+	    print( Li, Hi, Pi, k, A);	    
 	}
-	D[si] = pVal;
-    }
 
-    public String toString(){
-	return Arrays.toString(D);
+	//now Hi and Li have met at some point in the middle
+	//so we need to swap the pivot value into... 
+	//its correct place in the middle of the array
+	if (A[Li]>P){
+	    A[Pi] = A[Li];
+	    A[Li] = P;
+	    Pi = Li;
+	}
+	else {
+	    //if A[Li] is smaller thatn the pivot value...
+	    //put the pivot value to the right A[Li]
+	    A[Pi] = A[Li+1];
+	    A[Li+1] = P;
+	    Pi = Li+1;
+	}
+
+	print( Li, Hi, Pi, k, A);	    
+
+	//either found OR retry with smaller portion of array
+	System.out.println("k="+k+","+"Pi="+Pi+"!!!");
+	if (k==Pi){
+	    System.out.println("FOUND!");
+	}
+	else{
+	    if (k<Pi){
+		System.out.println("k is LESS");
+		select(A,k,L,Pi-1);
+	    }
+	    else {
+		System.out.println("k is GREATER");
+		select(A,k,Pi+1,H);
+	    }
+	}
+
+	/*
+	//either found OR retry with a smaller array
+	if (k==Pi){
+	    //System.out.println("Found!");
+	    System.out.printf("The %dth item is %d",k,A[Pi]);
+	    System.out.println();
+	}
+	else{
+	    if (k<Pi){
+		System.out.println("HEY"+(Pi));
+		select(A,k,L,Pi-1);
+		//redo with the part of the array before Pi
+	    }
+	    else{
+		System.out.println("HEy"+(Pi-1));
+		select(A,k,Pi+1,H);
+	    	
+		
+		//redo with the part of the array after  Pi
+	    }
+	}
+	*/
+	    
     }
     
-    //---------------[MAIN]----------------
-    public static void main(String[] args){
-	int[] a = {5,10,2,3,4,8,9,2,7};
-	Sort s = new Sort();
-	s.pivot(a);
+    public void print(int Li, int Hi, int Pi, int k, int[] A){
+	String s = String.format("Li=%d, Hi=%d, Pi=%d, k=%d",Li,Hi,Pi,k);
+	s+="\n";
+	s+=Arrays.toString(A);
+	s+="\n";
 	System.out.println(s);
     }
-	
+
+    //-------------------[MAIN]-----------------------
+    public static void main(String[] args){
+	Sort s = new Sort();
+	s.select(3);
+    }
 }
---
+++ 05-selection/oldAlgorithm/Sort.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Sort{
+    
+    int[] L; //input unsorted array
+    int[] D; //array I create to do stuff in
+    int pVal; //pivot value
+
+    public void pivot(int[] a){
+	L = a;
+	D = new int[a.length];
+	pVal = a[0];
+	pivot(a,0,a.length-1); 
+    }
+
+
+    public void pivot(int[] a, int si, int ei){
+	int i = 1; //si=1 because we are ignoring the first val of the array, the pVal
+	while (si != ei){
+	    if (L[i] < pVal){
+		D[si]=L[i];
+		si++;
+	    }
+	    else{
+		D[ei] = L[i];
+		ei--;
+	    }
+	    System.out.println(L[i]);
+	    System.out.println(this);
+	    i++;
+	}
+	D[si] = pVal;
+    }
+
+    public String toString(){
+	return Arrays.toString(D);
+    }
+    
+    //---------------[MAIN]----------------
+    public static void main(String[] args){
+	int[] a = {5,10,2,3,4,8,9,2,7};
+	System.out.println("original array: "+Arrays.toString(a));
+	Sort s = new Sort();
+	s.pivot(a);
+	System.out.println(s);
+    }
+	
+}
