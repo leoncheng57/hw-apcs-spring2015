878b1ae739fe4c925861f5e2ad7f7e42e656e844
leoncheng57 <57leonardo@gmail.com>
2015-03-14 20:12:02 -0400

    finished hw

--- 07-LL1/Driver.java
+++ 07-LL1/Driver.java
@@ -7,10 +7,23 @@ public class Driver{
 	    l.add(""+i);
 	}
 	System.out.println(l);
+	System.out.println();
 	System.out.println(l.find(0));
 	System.out.println(l.find(5));
 	System.out.println(l.find(7));
-	System.out.println(l.find(8));
-	System.out.println(l.find(-8));
+	// System.out.println(l.find(8));
+	// System.out.println(l.find(-8));
+	System.out.println();
+	l.insert(0,"a");
+	System.out.println(l);	
+	l.insert(1,"b");
+	System.out.println(l);	
+	l.insert(3,"c");
+	System.out.println(l);	
+	// l.insert(-3,"c");
+	// System.out.println(l);	
+	// l.insert(99,"c");
+	// System.out.println(l);	
+	
     }
 }
--- 07-LL1/LList.java
+++ 07-LL1/LList.java
@@ -7,7 +7,21 @@ public class LList{
 	l = tmp;
     }
 
+    
+    //will not work if n is negative or out of range
     public Node find(int n){
+	Node tmp = l;
+	while (n>0){
+	    tmp = tmp.getNext();
+	    n--;
+	}
+	return tmp;	
+    }
+    
+
+    //WILL work if n is negative or out of range
+    /*
+      public Node find(int n){
 	Node tmp = l;
 	if (n<0)
 	    System.out.print("BAD! the index is negative!");
@@ -20,12 +34,24 @@ public class LList{
 	    }
 	}
 	return tmp;	
-	    
-	
     }
+    */
 
-
-
+    //will not work if n is negative or out of range
+    public void insert(int n, String s){
+	if (n==0)
+	    add(s);
+	else{
+	    Node tmp = l;
+	    for (n=n;n>1;tmp=tmp.getNext()){
+		n--;
+	    }
+	    Node newbie = new Node(s);
+	    newbie.setNext(tmp.getNext());
+	    tmp.setNext(newbie);
+	}
+    }
+    
     public String toString(){
 	String s = "";
 	Node tmp;;
