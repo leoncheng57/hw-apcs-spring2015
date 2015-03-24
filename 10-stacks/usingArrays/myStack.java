7f12c48c664e6143d3a24e729750f68f27b1705a
leoncheng57 <57leonardo@gmail.com>
2015-03-24 09:25:47 -0400

    working

--- 10-stacks/usingArrays/myStack.java
+++ 10-stacks/usingArrays/myStack.java
@@ -5,26 +5,44 @@ import java.util.*;
 
 public class myStack<E>{
     
-    private ArrayList<E> al;
+    //private ArrayList<E> al;
+    private Object[] a;
     
     public myStack(){
-	al =  new ArrayList<E>();
+	//al =  new ArrayList<E>();
+	a = new Object[0];
     }
-
+    
+    public void extend(){
+	Object[] tmp = new Object[a.length+1];
+	for (int i =0 ;i<a.length;i++){
+	    tmp[i+1]=a[i];
+	}
+    }
+    
     public void push(E s){
-	al.add(0,s);
+	//al.add(0,s);
+	a.extend();
+	a[0]=s;
     }
 
     public E pop(){
-	return al.remove(0);
+	//return al.remove(0);
+	Object[] tmp = new Object[a.length-1];
+	for (int i =1 ;i<a.length;i++){
+	    tmp[i]=a[i-1];
+	}	
+	return a[0];
     }
 
     public boolean empty(){
-	return al.size() == 0;
+	//return al.size() == 0;
+	return a.length==0;
     }
 
     public E top(){
-	return al.get(0);
+	//return al.get(0);
+	return a[0];
     }
     
     public String toString(){
