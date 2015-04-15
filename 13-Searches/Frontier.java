b110f2916e78e12a9c21d91b94592c18f44f5c2c
leoncheng57 <57leonardo@gmail.com>
2015-04-15 09:07:53 -0400

    L

--- 13-Searches/Frontier.java
+++ 13-Searches/Frontier.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 public class Frontier {
-		public LinkedList<Node> l = new LinkedList<Node>();
+    public LinkedList<Node> l = new LinkedList<Node>();
 
-		public void add(Node n){
-				l.add(n);
-		}
+    public void add(Node n){
+	l.add(n);
+    }
 
-		public Node remove() {
-				return l.remove(0);
-		}
+    public Node remove() {
+	return l.remove(0);
+    }
 
-		public boolean isEmpty(){
-				return l.isEmpty();
-		}
+    public boolean isEmpty(){
+	return l.isEmpty();
+    }
 }
 
 
