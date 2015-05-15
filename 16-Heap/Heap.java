b31f4750265538dac257832a0d6e1df0013cb312
koding user <57leonardo@gmail.com>
2015-05-15 13:25:32 +0000

    L

--- 16-Heap/Heap.java
+++ 16-Heap/Heap.java
@@ -1,15 +1,43 @@
+import java.util.*;
+import java.io.*;
+
 public class Heap{
   
-  int[] storage;
+  //write heapsort!!!
+  
+  private ArrayList<Integer> data;
   
   public Heap(){
-      
+    data = new ArrayList<Integer>(); 
   }
   
-  //write heapsort
+  public void insert(int n){
+    data.add(n);
+    siftUp(n);
+  }  
+    
+  public void siftUp(int x){
+    int hold = data.indexOf(x);
+    int parentIndex=hold/2;
+    while (data.get(parentIndex)>data.get(hold)){
+        int tmp = data.get(parentIndex);
+        data.set(parentIndex,data.get(hold));
+        data.set(hold,tmp);
+        hold = parentIndex;
+        parentIndex = parentIndex/2;
+    }
+  }
   
+  public String toString(){
+    return data+"";
+  }
   
   public static void main (String[] args) {
-    /* code */
+    Heap h = new Heap();
+    h.insert(200);
+    h.insert(213);
+    h.insert(201);
+    h.insert(13);
+    System.out.println(h);
   }
 }
