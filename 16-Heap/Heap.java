dd59725b9880b51a5832dda38fcc936289ddf676
koding user <57leonardo@gmail.com>
2015-05-17 22:37:25 +0000

    sort not yet working

--- 16-Heap/Heap.java
+++ 16-Heap/Heap.java
@@ -26,22 +26,42 @@ public class Heap{
     }
   }
   
-  public pushDown(int n){
+  public int removeMin(){
+    int ret = data.get(0);
+    int n = data.remove(data.size()-1);
+    data.set(0,n);
+    pushDown(n);
+    return ret;
+  }
+  
+  public void pushDown(int n){
     int hold = data.indexOf(n);
     int leftChildIndex = hold*2;
     int rightChildIndex = hold*2+1;
-    while(data.get(hold)>data.get(leftChildIndex) || data.get(hold)>data.get(rightChildIndex)){
+    while(leftChildIndex<data.size() && rightChildIndex<data.size() &&
+      (data.get(hold)>data.get(leftChildIndex) || data.get(hold)>data.get(rightChildIndex))){
       if (data.get(leftChildIndex)<data.get(rightChildIndex)){
         int tmp = data.get(hold);
         data.set(hold,data.get(leftChildIndex));
         data.set(leftChildIndex,tmp);
+        hold = leftChildIndex;
       }
       else{
         int tmp = data.get(hold);
         data.set(hold,data.get(rightChildIndex));
         data.set(rightChildIndex,tmp);
+        hold = rightChildIndex;
       }
+      leftChildIndex = hold*2;
+      rightChildIndex = hold*2+1;
     }
+  }
+  
+  public String heapSort(ArrayList<Integer> intArrayList){
+    for (int i = intArrayList.size()-1;i>=0;i--){
+      pushDown(intArrayList.get(i));
+    }
+    return ""+intArrayList;
   }
   
   public String toString(){
@@ -56,5 +76,14 @@ public class Heap{
     h.insert(213);
     h.insert(2011);
     System.out.println(h);
+    h.removeMin();
+    System.out.println(h);
+    int[] elements = {12,312,3463,62,52235,262,52,123432};
+    ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+    for (int element : elements){
+      intArrayList.add(element);
+    }
+    System.out.println(intArrayList);
+    h.heapSort(intArrayList);
   }
 }
